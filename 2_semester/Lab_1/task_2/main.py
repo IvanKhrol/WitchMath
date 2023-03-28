ea9b443c06c4153df96221d22e9059e4783ce91c
IvanKhrol <khrol_ivan@mail.ru>
2023-03-28 15:48:57 +0300

    add some ch

--- 2_semester/Lab_1/task_2/main.py
+++ 2_semester/Lab_1/task_2/main.py
@@ -3,20 +3,21 @@ import matplotlib.pyplot as plt
 from sympy import *
 from methods import *
 
-numOfIter = 30000
+numOfIter = 300000
 beg_t, end_t = 0, 16
 step = (end_t - beg_t)/numOfIter
 
 m = 0.01227747
 M = 1 - m
-f_0 = 0 
+f_0 = 0
 
-begin = np.array([0.994, 0, 0, -2.031732629557337])
+begin = np.array([1.2, 0, 0, -1.05])
+# begin = np.array([0.994, 0, 0, -2.031732629557337])
 var('x, y, vx, vy, t') 
 r_1 = ((x + m)**2 + y**2)**(1/2)
 r_2 = ((x - M)**2 + y**2)**(1/2)
-dvx = 2*vy + x - M*(x+m)/r_1**3 - m * (x - M)/r_2**3 - f_0 * vx
-dvy = -2*vx + y - M * y/r_1**3 - m * y/r_2**3 - f_0 * vy
+dvx =  2*vy + x - M*(x+m)/(r_1)**3 - m * (x - M)/(r_2)**3 - f_0 * vx
+dvy = -2*vx + y - M * y/(r_1)**3 - m * y/(r_2)**3 - f_0 * vy
 f =  Array([vx, vy, dvx, dvy])
 func = lambdify((t, [x, y, vx, vy]), f, 'numpy')
 
@@ -29,7 +30,7 @@ else:
     print("Wrong input")
     exit(-1)
 
-plt.plot([item[0] for item in points], [item[1] for item in points])
+plt.plot([item[0] for item in points], [-item[1] for item in points])
 plt.xlabel('x')
 plt.ylabel('y')
 plt.grid()
